43fcc73690bb335cce623c1a6871df4b5b6377bd
Alikhan Batchaev <loko-han@mail.ru>
2019-11-09 15:29:20 +0300

    Initial Commit

---
+++ Bullseye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by 🧔🏻Alikhan Batchaev on 09.11.2019.
+//  Copyright © 2019 Alikhan Batchaev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
